cfb505523ea6e9c45703a787eed86c7f59f328a2
shobhit2001 <shobhitanand30@gmail.com>
2023-09-15 18:10:02 +0530

    PArt---6

--- dom.js
+++ dom.js
@@ -67,3 +67,21 @@ for(var i = 0; i < li.length; i++)
 var header = document.querySelector('#main-header');
 // console.log(header);
 header.style.borderBottom = 'solid 4px brown';
+
+var input = document.querySelector('input');
+input.value = 'Hello World'; 
+
+var submit = document.querySelector('input[type="submit"]');
+submit.value = "SEND";
+
+var item = document.querySelector('.list-group-item');
+item.style.color = 'red';
+
+var lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.color = 'blue';
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor = 'green';
+
+var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.display = 'none';
